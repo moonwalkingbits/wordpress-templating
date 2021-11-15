6a721247e1f66de959513f8db05841bbed5173e2
Martin Pettersson <martin@moonwalkingbits.com>
2021-11-15 08:51:10 +0100

    Initial commit

---
+++ src/class-engine-interface.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Templating: Template engine interface
+ *
+ * @package Moonwalking_Bits\Templating
+ * @author Martin Pettersson
+ * @license GPL-2.0
+ * @since 0.1.0
+ */
+
+namespace Moonwalking_Bits\Templating;
+
+/**
+ * Represents a template engine.
+ *
+ * @since 0.1.0
+ */
+interface Engine_Interface {
+
+	/**
+	 * Adds the given list of directories to be searched for templates.
+	 *
+	 * @since 0.1.0
+	 * @param string[] $directories Directories to add to search path.
+	 */
+	public function add_template_directories( array $directories ): void;
+
+	/**
+	 * Renders a matching template in the given context.
+	 *
+	 * @since 0.1.0
+	 * @param string $template_name Name of the template to render.
+	 * @param array  $context Context to render the template in.
+	 * @return string Rendered template result.
+	 * @throws \Moonwalking_Bits\Templating\Template_Not_Found_Exception When the given template cannot be found.
+	 */
+	public function render( string $template_name, array $context = array() ): string;
+}
---
+++ src/class-template-not-found-exception.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Templating: Template nof found exception class
+ *
+ * @package Moonwalking_Bits\Templating
+ * @author Martin Pettersson
+ * @license GPL-2.0
+ * @since 0.1.0
+ */
+
+namespace Moonwalking_Bits\Templating;
+
+use Exception;
+
+/**
+ * An exception thrown when a template engine cannot find a given template file.
+ *
+ * @since 0.1.0
+ */
+class Template_Not_Found_Exception extends Exception {
+
+	/**
+	 * Creates a new exception instance.
+	 *
+	 * @since 0.1.0
+	 * @param string $template_name Name of the missing template file.
+	 */
+	public function __construct( string $template_name ) {
+		parent::__construct( "Could not find template: {$template_name}" );
+	}
+}
